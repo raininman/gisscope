9e2e5eb7faa5d46d47c42b54046e076cb597fff7
Egor Orlov <egororloff@gmail.com>
2023-09-22 16:20:50 +0300

    Login Page

---
+++ android/app/src/main/kotlin/com/example/gisscope/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gisscope
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:gisscope/login_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter Demo',
+      home: LoginPage(),
+    );
+  }
+}
